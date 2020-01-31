5c84f2d72cb6e0c10d963b40a34cc3a0c36b3b70
Léo <leoveyssiere.pro@hotmail.fr>
2020-01-31 23:22:36 +0100

    Feu : propagation

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public List<Transform> spawns;
+    public GameObject firePrefab;
+
+    public int dot;
+    public float timeToPropagate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Propagate", timeToPropagate, timeToPropagate);
+    }
+
+    public void Propagate()
+    {
+        int rdm;
+        //check pour savoir si y'a déjà un feu
+        bool fireDetected = false;
+        do
+        {
+            rdm = Random.Range(0, spawns.Count);
+            Collider[] colliders = Physics.OverlapSphere(transform.position + spawns[rdm].position, 0.2f, 8);
+            if (colliders.Length > 0)
+            {
+                Debug.Log("sdfdsfds");
+                fireDetected = true;
+            }
+        } while (fireDetected);
+
+        GameObject spawnedFire = Instantiate(firePrefab, spawns[rdm].position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int life; //vie du bateau
+    public GameObject problems;
+    public float BaseTimeToWait;
+
+    private float timeToWait;
+    private float timeElapsed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+    }
+}

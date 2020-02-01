958032b75ffd9d3cc0f21e881373065da60edd8a
Léo <leoveyssiere.pro@hotmail.fr>
2020-02-01 15:59:02 +0100

    Debut interact avec canon

---
+++ Assets/Scripts/HealBaril.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealBaril : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<PlayerInteract>().interactBaril = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<PlayerInteract>().interactBaril = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public float healAmount;
+    public bool interactBaril = false;
+
+    public void OnAction(InputValue value)
+    {
+        if (interactBaril)
+        {
+            GetComponent<PlayerHealthManager>().Heal(healAmount);
+        }
+    }
+}

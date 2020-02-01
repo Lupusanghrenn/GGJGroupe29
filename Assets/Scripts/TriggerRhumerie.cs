84b22c645950345f11d95432496fa8fa5cdfa6c0
Max Emsallem <emsallem.max@hotmail.Fr>
2020-02-01 21:49:49 +0100

    Added heal rhumerie

---
+++ Assets/Scripts/TriggerRhumerie.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerRhumerie : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.GetComponent<PlayerHealthManager>().Heal(other.GetComponent<PlayerHealthManager>().maxHealth/2);
+        }
+    }
+}

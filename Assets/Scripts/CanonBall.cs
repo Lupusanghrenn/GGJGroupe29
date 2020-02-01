e2d2c3ea660d6baddc538f9b68684a0853a61238
Max Emsallem <emsallem.max@hotmail.Fr>
2020-02-01 14:39:58 +0100

    Added shooting enemy and boulets de canon

---
+++ Assets/Scripts/CanonBall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanonBall : MonoBehaviour
+{
+    private void Start()
+    {
+        Destroy(this.gameObject, 3f);
+    }
+    void Update()
+    {
+        this.transform.position += new Vector3(35 * Time.deltaTime, 0, 0);
+    }
+}

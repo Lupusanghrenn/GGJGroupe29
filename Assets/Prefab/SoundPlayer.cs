db934b18a51f3054c356a451c7049e75c803fe07
Léo <leoveyssiere.pro@hotmail.fr>
2020-02-02 12:08:08 +0100

    SoundPlayer

---
+++ Assets/Prefab/SoundPlayer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    public void PlaySound(AudioClip audio)
+    {
+        GetComponent<AudioSource>().PlayOneShot(audio);
+    }
+}
